8b8f47a5cfcc008153bfcba499e8854ff7d8764f
joshsellers <joshsellers7.5@gmail.com>
2026-08-12 14:35:59 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#wordle-solver<br>Solves Wordles
---
+++ wordle-solver.py
@@ -0,0 +1,104 @@
+word_length = 5
+words = []
+
+
+def load_words():
+    with open('words_alpha.txt', 'r') as words_file:
+        for line in words_file:
+            if len(line.strip()) == word_length: words.append(line.strip())
+
+
+letters = []
+excluded_letters = []
+excluded_indices = {}
+max_occurances = {}
+
+word_frame = ['' for i in range(0, word_length)]
+
+
+def make_guess():
+    result = input('Enter word and results: ')
+    if result.strip().lower() == 'exit':
+        return True
+
+    input_word = result.split(' ')[0]
+    data = result.split(' ')[1]
+
+    for i in range(0, len(input_word)):
+        if data[i] == '0':
+            if input_word[i] not in letters:
+                excluded_letters.append(input_word[i])
+            else:
+                if input_word[i] not in excluded_indices:
+                    excluded_indices[input_word[i]] = []
+                excluded_indices[input_word[i]].append(i)
+
+                max_occurances[input_word[i]] = 0
+                for j in range(0, len(input_word)):
+                    if j == i:
+                        break
+                    if input_word[j] == input_word[i]:
+                        max_occurances[input_word[i]] += 1
+
+        elif data[i] == '1':
+            if input_word[i] not in excluded_indices:
+                excluded_indices[input_word[i]] = []
+            excluded_indices[input_word[i]].append(i)
+            letters.append(input_word[i])
+
+            if input_word[i] in excluded_letters:
+                excluded_letters.pop(excluded_letters.index(input_word[i]))
+
+        elif data[i] == '2':
+            word_frame[i] = input_word[i]
+            letters.append(input_word[i])
+
+            if input_word[i] in excluded_letters:
+                excluded_letters.pop(excluded_letters.index(input_word[i]))
+
+    def test_word(word):
+        for char, indices in excluded_indices.items():
+            if char in word:
+                for index in indices:
+                    if word[index] == char:
+                        return False
+
+        for letter in excluded_letters:
+            if letter in word:
+                return False
+
+        for letter in letters:
+            if letter not in word:
+                return False
+
+        for letter in word_frame:
+            if letter != '' and word.index(letter) != word_frame.index(letter):
+                return False
+
+        for letter in max_occurances.keys():
+            occurances = 0
+            for character in word:
+                if character == letter:
+                    occurances += 1
+
+            if occurances > max_occurances[letter]:
+                return False
+
+        return True
+
+    candidate_words = []
+    for word in words:
+        if test_word(word):
+            candidate_words.append(word)
+
+    print(candidate_words)
+
+    return False
+
+
+if __name__ == '__main__':
+    load_words()
+
+    exit = False
+    while not exit:
+        exit = make_guess()
