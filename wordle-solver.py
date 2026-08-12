610630f3a5267ebbbaa185e712d84f98c1d80f74
joshsellers <joshsellers7.5@gmail.com>
2026-08-12 16:30:38 -0500

    fix more things

--- wordle-solver.py
+++ wordle-solver.py
@@ -16,6 +16,7 @@ letters = []
 excluded_letters = []
 excluded_indices = {}
 max_occurences = {}
+min_occurences = {}
 
 word_frame = []
 
@@ -23,9 +24,27 @@ word_frame = []
 def make_guess():
     global word_length
     global word_frame
+    global letters
+    global excluded_letters
+    global excluded_indices
+    global max_occurences
+    global min_occurences
+    global loaded_words
+    global words
+
     result = input('Enter word and results: ')
     if result.strip().lower() == 'exit':
         return True
+    elif result.strip().lower() == 'reset':
+        word_length = 5
+        words = []
+        letters = []
+        excluded_letters = []
+        excluded_indices = {}
+        max_occurences = {}
+        min_occurences = {}
+        word_frame = []
+        return False
 
     input_word = result.split(' ')[0]
     if not loaded_words:
@@ -46,9 +65,7 @@ def make_guess():
 
                 max_occurences[input_word[i]] = 0
                 for j in range(0, len(input_word)):
-                    if j == i:
-                        break
-                    if input_word[j] == input_word[i]:
+                    if input_word[j] == input_word[i] and data[j] != '0':
                         max_occurences[input_word[i]] += 1
 
         elif data[i] == '1':
@@ -66,6 +83,17 @@ def make_guess():
 
             if input_word[i] in excluded_letters:
                 excluded_letters.pop(excluded_letters.index(input_word[i]))
+
+    checked_letters = []
+    for i in range(word_length):
+        datum = data[i]
+        letter = input_word[i]
+        if datum == '1' and letter not in checked_letters:
+            min_occurences[letter] = 0
+            for j in range(word_length):
+                if input_word[j] == letter and data[j] != '0':
+                    min_occurences[letter] += 1
+            checked_letters.append(letter)
 
     def test_word(word):
         for char, indices in excluded_indices.items():
@@ -96,11 +124,20 @@ def make_guess():
             if occurences > max_occurences[letter]:
                 return False
 
+        for letter in min_occurences.keys():
+            occurences = 0
+            for character in word:
+                if character == letter:
+                    occurences += 1
+
+            if occurences < min_occurences[letter]:
+                return False
+
         return True
 
     candidate_words = []
     for word in words:
-        if test_word(word):
+        if test_word(word) and word not in candidate_words:
             candidate_words.append(word)
 
     print(candidate_words)
@@ -113,9 +150,12 @@ if __name__ == '__main__':
     exit = False
     while not exit:
         exit = make_guess()
+        print()
         if debug:
             print(f'letters: {letters}')
             print(f'excluded_letters: {excluded_letters}')
             print(f'excluded_indices: {excluded_indices}')
             print(f'max_occurences: {max_occurences}')
+            print(f'min_occurences: {min_occurences}')
             print(f'word_frame: {word_frame}')
+            print()
